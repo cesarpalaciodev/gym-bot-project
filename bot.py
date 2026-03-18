ca53121125d5cac0e0ab21868a08b5c88cbc2b68
Cesar Palacio <cesarpalaciodev@gmail.com>
2026-03-18 16:47:31 -0500

    fix supabase deps

--- bot.py
+++ bot.py
@@ -14,12 +14,15 @@ from supabase import create_client
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-
+if not SUPABASE_URL or not SUPABASE_KEY:
+    
+    raise ValueError("Faltan variables de entorno de Supabase")
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 # ==================================================
 # CONFIGURACION
 # ==================================================
+
 
 TOKEN = os.getenv("TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
